c0528a563bf053efb7c9aefac634c7201660b086
Samarin Daniil <daniil_samarin@mail.ru>
2022-04-06 16:10:05 +0700

    Fix board.c

--- src/libchessviz/board.c
+++ src/libchessviz/board.c
@@ -6,12 +6,12 @@ int check_board(int *symbol, char chess[9][9])
     //check final field
     symbol_x = symbol[3];
     symbol_y = symbol[4];
-    if (chess[symbol_x][symbol_y] == ' ')
+    if (chess[symbol_y][symbol_x] == ' ')
     {
         //check initial field
         symbol_x = symbol[0];
         symbol_y = symbol[1];
-        if (chess[symbol_x][symbol_y] != ' ')
+        if (chess[symbol_y][symbol_x] != ' ')
         {
             return 0;
         } else {
