00765e536036f51eeeba222e2fc8d342364fcec4
Samarin Daniil <daniil_samarin@mail.ru>
2022-04-06 16:08:26 +0700

    Fix main

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -13,47 +13,45 @@
 int main()
 {
     char chess[n][n] = {{' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
-                        {'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                        {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                        {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                        {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                         {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                        {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                        {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+                        {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'7', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                        {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
     int *symbol = (int*)malloc(5*sizeof(int));
     memset(symbol, 0, sizeof(int)*5);
     if(!symbol)
         return -1;
     char *step = (char*)malloc(6*sizeof(char));;
     print_board(chess);
-    scanf("\n%s", step);
-    if (step[0] == 'q')
-    {
-        printf("\n end");
-    }  
-    if (read_move(symbol, step))
-    {
-        for(int i = 0; i < 6; i++)
+    do {
+        scanf("\n%s", step);
+        if (step[0] == 'q')
         {
-            printf("%d", symbol[i]);
-        }
-        printf("\n");
-        if (check_board(symbol, chess))
+            printf(" end \n");
+            break;
+        }  
+        if (read_move(symbol, step) == 0)
         {
-            if (check_move(symbol, chess))
+            if (check_board(symbol, chess) == 0)
             {
-                move(symbol, chess);
-                print_board(chess);
+                if (check_move(symbol, chess) == 0)
+                {
+                    move(symbol, chess);
+                    print_board(chess);
+                } else {
+                    printf("\n error check-move \n");
+                }
             } else {
-                printf("error check-move");
+                printf("\n error check-board \n");
             }
         } else {
-            printf("error check-board");
+            printf("\n error read-move \n");
         }
-    } else {
-        printf("error read-move");
-    }
+    }while (1);
     free(step);
     free(symbol);
     return 0;
